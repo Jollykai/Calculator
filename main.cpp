9dfc83e031ffc4ef21cff83d3f5ab376368869c4
Losthero <ist.magna750@gmail.com>
2022-03-14 00:12:26 +0300

    Get sum

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//setlocale(0,"");
+	int a, b;
+	cout << "Enter first num:";
+	cin >> a;
+	cout << "Enter second num:";
+	cin >> b;
+	int c = a + b;
+	cout << "Sum of numbers = " << c << endl;
+	return 0;	
+}
